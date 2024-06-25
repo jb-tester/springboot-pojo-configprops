ef29e9702814d1a65c4716ed1e36b0f9dd467bac
Irina <irina.petrovskaya@jetbrains.com>
2024-06-25 09:48:56 +0200

    readme.md

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,5 +11,14 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
+@Bean
+    public CommandLineRunner commandLineRunner(MyProps myProps) {
+        return args -> {
+            System.out.println("--------------------------------------");
+			System.out.println(myProps.getFoo().getBar());
+			System.out.println(myProps.getFoolist().get(0).getBar());
+			System.out.println(myProps.getInnerfoo().getBuzz());
+			System.out.println("--------------------------------------");
+        };
+    }
 }
