7f04ed239faba0075eb991735d4a901e6758a4ed
Irina.Petrovskaya <irina.petrovskaya@jetbrains.com>
2026-04-21 17:10:58 +0200

    add the library project to check the library class-type properties of the @ConfigurationProperties class

---
+++ pojos-lib/src/main/java/com/mytests/spring/LibPojo1.java
@@ -0,0 +1,35 @@
+package com.mytests.spring;
+
+
+import java.util.Map;
+
+public class LibPojo1 {
+
+    String libProp1;
+    String libProp2;
+    Map<String,String> libProp3;
+
+    public String getLibProp1() {
+        return libProp1;
+    }
+
+    public void setLibProp1(String libProp1) {
+        this.libProp1 = libProp1;
+    }
+
+    public String getLibProp2() {
+        return libProp2;
+    }
+
+    public void setLibProp2(String libProp2) {
+        this.libProp2 = libProp2;
+    }
+
+    public Map<String, String> getLibProp3() {
+        return libProp3;
+    }
+
+    public void setLibProp3(Map<String, String> libProp3) {
+        this.libProp3 = libProp3;
+    }
+}
--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
 https://youtrack.jetbrains.com/issue/IDEA-323234
+
+
+Library class-type property using:
+- import the `pojos-lib` project and run `mvn install` for it
+- unlink this project
+- import `springboot-pojo-configprops`
