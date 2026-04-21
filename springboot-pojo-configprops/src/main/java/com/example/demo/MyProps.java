765f276210360bba3a0fb5ca579547dcdacd2a10
Irina.Petrovskaya <irina.petrovskaya@jetbrains.com>
2026-04-21 17:11:18 +0200

    add the library project to check the library class-type properties of the @ConfigurationProperties class

---
+++ springboot-pojo-configprops/src/main/java/com/example/demo/MyProps.java
@@ -0,0 +1,88 @@
+package com.example.demo;
+
+import com.mytests.spring.LibPojo1;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+
+@Component
+@ConfigurationProperties("my.props")
+public class MyProps {
+
+  //  @NestedConfigurationProperty // annotation is required here
+    Foo foo;
+    //  annotation is not required for collection of external pojos:
+    List<Foo> foolist;
+    //  annotation is not required for the inner class
+    InnerFoo innerfoo;
+    // annotation is not required, but the null value will be applied
+    NonStaticInnerFoo nonStaticInnerFoo;
+    //@NestedConfigurationProperty // annotation is required here
+    LibPojo1 libPojo1;
+
+    public static class InnerFoo{
+        String buzz;
+
+        public String getBuzz() {
+            return buzz;
+        }
+
+        public void setBuzz(String buzz) {
+            this.buzz = buzz;
+        }
+    }
+
+    public class NonStaticInnerFoo{
+        String buzz;
+
+        public String getBuzz() {
+            return buzz;
+        }
+
+        public void setBuzz(String buzz) {
+            this.buzz = buzz;
+        }
+    }
+    public Foo getFoo() {
+        return foo;
+    }
+
+    public void setFoo(Foo foo) {
+        this.foo = foo;
+    }
+
+    public List<Foo> getFoolist() {
+        return foolist;
+    }
+
+    public void setFoolist(List<Foo> foolist) {
+        this.foolist = foolist;
+    }
+
+    public InnerFoo getInnerfoo() {
+        return innerfoo;
+    }
+
+    public void setInnerfoo(InnerFoo innerfoo) {
+        this.innerfoo = innerfoo;
+    }
+
+    public NonStaticInnerFoo getNonStaticInnerFoo() {
+        return nonStaticInnerFoo;
+    }
+
+    public void setNonStaticInnerFoo(NonStaticInnerFoo nonStaticInnerFoo) {
+        this.nonStaticInnerFoo = nonStaticInnerFoo;
+    }
+
+    public LibPojo1 getLibPojo1() {
+        return libPojo1;
+    }
+
+    public void setLibPojo1(LibPojo1 libPojo1) {
+        this.libPojo1 = libPojo1;
+    }
+}
